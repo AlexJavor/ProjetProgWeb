78696eb8169a07c982c22ee2005c88e80cef369c
M. Salinas Gohin Anthony <salinasg@insa-20232>
2018-12-10 18:21:18 +0100

    Modifications code slides

--- script_js/main_accueil.js
+++ script_js/main_accueil.js
@@ -1,15 +1,18 @@
 function showDivs(n) {
     let x = document.getElementsByClassName("slides");
-    if (n > x.length) {slideIndex = 1}
+    if (n > x.length) {slideIndex = 1};
     if (n < 1) {slideIndex = x.length} ;
     for (let i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+    console.log(x);
     x[slideIndex-1].style.display = "block";
-    }
+}
 
-var slideIndex = 1;
-showDivs(slideIndex);
+window.onload = function(){
+    var slideIndex = 1;
+    showDivs(slideIndex);
+}
 
 function plusDivs(n){
     showDivs(slideIndex += n);
