e647975c061b66a5d864c755b176403cdcfaed51
M. Javornik Alexandre <javornik@insa-08491>
2018-12-14 17:03:33 +0100

    Reparation bugs

--- script_js/main_univ.js
+++ script_js/main_univ.js
@@ -1,12 +1,22 @@
 window.addEventListener('scroll', function(){
     const logoINSA = document.querySelector(".logo_INSA img");
     const mainNav = document.getElementById("mainNav");
+    const listeuniv = document.getElementsByClassName("Liste_univs");
+    var i;
     if (window.pageYOffset > 0){
         logoINSA.style.height = "80px";
-        mainNav.classList.add('bg-black');
+        //mainNav.classList.add('bg-black');
+        mainNav.style.background = "#333";
+        for (i = 0; i < 4; i++){
+            listeuniv[i].style.background = "#333";
+        }
+        console.log(listeuniv);
     } else{
         logoINSA.style.height = "100px";
-        mainNav.classList.remove('bg-black');
-
+        //mainNav.classList.remove('bg-black');
+        mainNav.style.background = "#eee";
+        for (i = 0; i < 4; i++){
+            listeuniv[i].style.background = "#eee";
+        }
     }
 });
